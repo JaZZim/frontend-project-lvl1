734256493129d64b0f043aedc983246f4c9c0407
Anton Nikulin <hotjazzim@gmail.com>
2021-01-27 18:11:11 +0500

    add Brain-calc game

---
+++ src/games/calc.js
@@ -0,0 +1,27 @@
+import getRandomInt from '../utils/random.js';
+import runGameEngine from '../index.js';
+
+const gameRules = 'What is the result of the expression?';
+
+const getCurrectAnswer = (operator, firstArgument, secondArgument) => {
+  switch (operator) {
+    case '-':
+      return `${firstArgument - secondArgument}`;
+    case '*':
+      return `${firstArgument * secondArgument}`;
+    default:
+      return `${firstArgument + secondArgument}`;
+  }
+};
+
+const getCalcConditions = () => {
+  const operators = ['+', '-', '*'];
+  const randomOperator = operators[getRandomInt(0, 2)];
+  const firstArgument = getRandomInt(1, 50);
+  const secondArgument = getRandomInt(1, 50);
+  const correctAnswer = getCurrectAnswer(randomOperator, firstArgument, secondArgument);
+  const question = `${firstArgument} ${randomOperator} ${secondArgument}`;
+  return [question, correctAnswer];
+};
+
+export default () => runGameEngine(gameRules, getCalcConditions);
