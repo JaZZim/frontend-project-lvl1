7e07403eecda25d596de62633bfde5832dadae77
Anton Nikulin <hotjazzim@gmail.com>
2021-01-28 15:49:23 +0500

    refactoring even.js

--- src/games/even.js
+++ src/games/even.js
@@ -3,14 +3,12 @@ import runGameEngine from '../index.js';
 
 const isEven = (number) => number % 2 === 0;
 
-const getCorrectAnswer = (number) => (isEven(number) ? 'yes' : 'no');
-
 const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const getEvenConditions = () => {
-  const numberForRound = getRandomInt();
-  const correctAnswer = getCorrectAnswer(numberForRound);
-  return [numberForRound, correctAnswer];
+  const randomNumber = getRandomInt();
+  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+  return [randomNumber, correctAnswer];
 };
 
 export default () => runGameEngine(gameRules, getEvenConditions);
