c3e265f4e810b3e764b0dd169e028d49daf1676f
Anton Nikulin <hotjazzim@gmail.com>
2021-01-29 16:32:52 +0500

    move logic of greeting to the engine

--- src/index.js
+++ src/index.js
@@ -1,27 +1,23 @@
 import readlineSync from 'readline-sync';
-import sayHelloAndGetUserName from './cli.js';
-
-const askQuestion = (question) => console.log(`Question: ${question}`);
-
-const getAnswer = () => readlineSync.question('Your answer: ');
-
-const isCorrectAnswer = (correctAnswer, answer) => correctAnswer === answer;
 
 const runGameEngine = (gameRules, getGameConditions) => {
-  const playerName = sayHelloAndGetUserName();
   const countOfRoundsGame = 3;
+
+  console.log('Welcome to the Brain Games!');
+  const playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
   console.log(gameRules);
+
   for (let round = 1; round <= countOfRoundsGame; round += 1) {
     const [question, correctAnswer] = getGameConditions();
-    askQuestion(question);
-    const playerAnswer = getAnswer();
-    if (isCorrectAnswer(correctAnswer, playerAnswer)) {
-      console.log('Correct!');
-    } else {
+    console.log(`Question: ${question}`);
+    const playerAnswer = readlineSync.question('Your answer: ');
+    if (correctAnswer !== playerAnswer) {
       console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${playerName}!`);
       return;
     }
+    console.log('Correct!');
   }
   console.log(`Congratulations, ${playerName}!`);
 };
