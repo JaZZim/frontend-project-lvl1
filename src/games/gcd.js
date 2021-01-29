695baadc1b2c8ecda263dc2a7b3f8d5d3700f4a8
Anton Nikulin <hotjazzim@gmail.com>
2021-01-29 16:40:14 +0500

    refactoring calcGcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,22 +1,23 @@
-import getRandomInt from '../utils/random.js';
+import getRandomInt from '../getRandomInt.js';
 import runGameEngine from '../index.js';
+
+const randomMin = 0;
+const randomMax = 100;
 
 const gameRules = 'Find the greatest common divisor of given numbers.';
 
-const calcGcd = (firstArgument, secondArgument) => {
-  const min = Math.min(firstArgument, secondArgument);
-  const max = Math.max(firstArgument, secondArgument);
-  if (min === 0) {
-    return max;
+const calcGcd = (int1, int2) => {
+  if (int1 === 0) {
+    return int2;
   }
-  return calcGcd(max % min, min);
+  return calcGcd(int2 % int1, int1);
 };
 
 const getGcdConditions = () => {
-  const firstArgument = getRandomInt();
-  const secondArgument = getRandomInt();
-  const correctAnswer = calcGcd(firstArgument, secondArgument);
-  const question = `${firstArgument} ${secondArgument}`;
+  const num1 = getRandomInt(randomMin, randomMax);
+  const num2 = getRandomInt(randomMin, randomMax);
+  const correctAnswer = calcGcd(num1, num2);
+  const question = `${num1} ${num2}`;
   return [question, `${correctAnswer}`];
 };
 
